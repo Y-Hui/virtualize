c64ca19f29faefdb3ddc435840748a7620f53eaa
Y-Hui <y-hui.mail@foxmail.com>
2024-12-10 20:59:47 +0800

    🔧 build: demo base

--- scripts/build-demo/index.ts
+++ scripts/build-demo/index.ts
@@ -4,6 +4,6 @@ import { cleanDir } from '../utils/clean-dir'
 import { mv } from '../utils/move'
 import { runCommand } from '../utils/run-command'
 
-runCommand('pnpm run build --base=/virtualize/dist', genPath('packages/playground'))
+runCommand('pnpm run build --base=/virtualize/', genPath('packages/playground'))
 cleanDir(genPath('/dist'))
 mv(genPath('packages/playground/dist'), genPath('/dist'))
