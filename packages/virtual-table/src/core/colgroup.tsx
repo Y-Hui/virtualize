4bab574cb023b7c58adcd3c4a69556ea7a69e85f
Y-Hui <y-hui.mail@foxmail.com>
2025-05-01 00:04:55 +0800

    🐞 fix(virtual-table): 修复列宽计算在弹窗中可能获取到 0 的 bug

--- packages/virtual-table/src/core/colgroup.tsx
+++ packages/virtual-table/src/core/colgroup.tsx
@@ -1,6 +1,6 @@
 import type { FC, Key } from 'react'
 import type { ColumnDescriptor } from './types'
-import { memo, useRef } from 'react'
+import { memo, useLayoutEffect, useRef } from 'react'
 import { isShallowEqual } from '../utils/equal'
 
 export interface ColgroupProps {
@@ -12,19 +12,24 @@ const Colgroup: FC<ColgroupProps> = (props) => {
   const { columns, onColumnSizesMeasure } = props
 
   const enableMeasure = onColumnSizesMeasure != null
-  const columnSizes = new Map<Key, number>()
+  const columnSizes = new Map<Key, () => number>()
   const prevColumnSizes = useRef(new Map<Key, number>())
 
+  useLayoutEffect(() => {
+    if (!enableMeasure) return
+    const sizes = new Map<Key, number>()
+    columnSizes.forEach((item, key) => {
+      sizes.set(key, item())
+    })
+    if (!isShallowEqual(sizes, prevColumnSizes.current)) {
+      console.log(sizes)
+      onColumnSizesMeasure(sizes)
+      prevColumnSizes.current = sizes
+    }
+  })
+
   return (
-    <colgroup
-      ref={() => {
-        if (!enableMeasure) return
-        if (!isShallowEqual(columnSizes, prevColumnSizes.current)) {
-          onColumnSizesMeasure(columnSizes)
-          prevColumnSizes.current = columnSizes
-        }
-      }}
-    >
+    <colgroup>
       {columns.map((item) => {
         const { key } = item
         if (item.type === 'blank') {
@@ -38,8 +43,10 @@ const Colgroup: FC<ColgroupProps> = (props) => {
             key={key}
             ref={(node) => {
               if (node == null || !enableMeasure) return
-              columnSizes.set(key, node.offsetWidth)
+              // eslint-disable-next-line react-compiler/react-compiler
+              columnSizes.set(key, () => node.offsetWidth)
             }}
+            data-col-key={key}
             style={{
               width: column.width,
               minWidth: column.minWidth,
